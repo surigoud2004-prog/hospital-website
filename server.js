467d8d0e4d2f8d6075278d37646f552e818e7505
SURESH GOUD <surigoud2004@gmail.com>
2026-04-10 19:26:03 +0530

    recorected

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.post('/login', (req, res) => {
 // At the bottom of server.js
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0',() => {
     console.log(`Server is running on port ${PORT}`);
 });
