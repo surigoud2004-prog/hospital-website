9b70f1d1c5dc67ab4987f0accc86510e52dac6cc
SURESH GOUD <surigoud2004@gmail.com>
2026-04-10 18:54:40 +0530

    added

--- server.js
+++ server.js
@@ -16,4 +16,9 @@ app.post('/login', (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Security server running on http://localhost:3000'));
+// At the bottom of server.js
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
