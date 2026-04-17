4e225980ec8d44f65982a7cb10fa35b520820e85
SURESH GOUD <surigoud2004@gmail.com>
2026-04-17 19:11:19 +0530

    restored

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/login', (req, res) => {
     }
 });
 
-app.get('(.*)',(req,res)=>{
+app.get('/:splat*',(req,res)=>{
     res.sendFile(path.join(__dirname,'dist','index.html'))
 })
 // At the bottom of server.js
